7c1f82e62bf8717d2033550c7e94ce82e38eef4a
thehamzakargin <hamzakargin@outlook.com>
2023-11-24 22:57:02 +0300

    handle forms duration

---
+++ src/components/BookEdit.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function BookEdit({ book, onEdit }) {
+    const [title, setTitle] = useState(book.title);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onEdit(book.id, title)
+    }
+
+    return(
+    <form onSubmit={handleSubmit} className="book-edit">
+        <label>
+            title
+        </label>
+        <input className="input" value={title} onChange={handleChange}/>
+        <button className="button is-primary">
+            Save
+        </button>
+    </form>
+    )
+}
+
+export default BookEdit;
